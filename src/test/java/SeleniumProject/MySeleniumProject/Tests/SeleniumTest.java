ac41f3f858fd0791874c8618c99b284d8c827e58
harpreetsindhia999@gmail.com <harpreetsindhia999@gmail.com>
2024-07-05 17:41:28 +0530

    Initial Commit made by Original Sara Williams

---
+++ src/test/java/SeleniumProject/MySeleniumProject/Tests/SeleniumTest.java
@@ -0,0 +1,13 @@
+package SeleniumProject.MySeleniumProject.Tests;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void masterSelenium() {
+		
+		System.out.println("Master Selenium framework by working on good projects");
+	}
+
+}
