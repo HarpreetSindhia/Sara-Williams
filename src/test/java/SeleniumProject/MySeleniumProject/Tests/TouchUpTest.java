d140115d7eee8ee4f64a17fb2d1b817f83d90077
harpreetsindhia999@gmail.com <harpreetsindhia999@gmail.com>
2024-07-05 17:55:41 +0530

    Commit made by Architect

---
+++ src/test/java/SeleniumProject/MySeleniumProject/Tests/TouchUpTest.java
@@ -0,0 +1,13 @@
+package SeleniumProject.MySeleniumProject.Tests;
+
+import org.testng.annotations.Test;
+
+public class TouchUpTest {
+	
+	@Test
+	public void Testing() {
+		
+		System.out.println("Through testing has been done to implement the project");
+	}
+
+}
